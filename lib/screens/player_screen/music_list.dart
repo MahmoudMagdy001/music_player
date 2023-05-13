b5bc3bf792c6724a76c5057abe29f061c5d66b02
MahmoudMagdi001 <urfavdodi@gmail.com>
2023-05-13 15:23:00 +0300

    eeeeeeeeeeennnnnnnnnnnnnnnnnddddddddddddddddd

--- lib/screens/player_screen/music_list.dart
+++ lib/screens/player_screen/music_list.dart
@@ -5,13 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:music_player/screens/auth_screen/widgets/custom_text_form_field.dart';
-import 'package:music_player/screens/auth_screen/auth_screen.dart';
 import 'package:music_player/screens/player_screen/data/position_data.dart';
+import 'package:music_player/screens/player_screen/widgets/custo_drawer.dart';
 import 'package:music_player/screens/player_screen/widgets/player_screen.dart';
-import 'package:provider/provider.dart';
 import 'package:rxdart/rxdart.dart';
-
-import '../auth_screen/data/google_signin.dart';
 
 class AudioPlayerScreen extends StatefulWidget {
   const AudioPlayerScreen({Key? key}) : super(key: key);
@@ -150,98 +147,7 @@ class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
           ),
         ),
       ),
-      drawer: Drawer(
-        backgroundColor: const Color(0xFF144771),
-        elevation: 100,
-        child: ListView(
-          physics: const BouncingScrollPhysics(),
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              decoration: const BoxDecoration(color: Color(0xFF071A2C)),
-              accountName: Text(
-                user!.displayName ?? '',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              accountEmail: Text(
-                'Email: ${user!.email ?? ''}',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              currentAccountPicture: CircleAvatar(
-                foregroundColor: Colors.white,
-                radius: 40,
-                backgroundImage: user!.photoURL != null
-                    ? NetworkImage(user!.photoURL!)
-                    : const NetworkImage(
-                        'https://previews.123rf.com/images/tuktukdesign/tuktukdesign1606/tuktukdesign160600119/59070200-user-icon-man-profil-homme-d-affaires-avatar-personne-ic%C3%B4ne-illustration-vectorielle.jpg',
-                      ),
-              ),
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.settings_outlined,
-                color: Colors.white,
-              ),
-              title: const Text(
-                'Account Settings',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onTap: () {
-                // Add navigation code here
-              },
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.info_outline,
-                color: Colors.white,
-              ),
-              title: const Text(
-                'Help',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onTap: () {
-                // Add navigation code here
-              },
-            ),
-            const SizedBox(height: 10),
-            const Divider(color: Colors.white),
-            const SizedBox(height: 10),
-            ListTile(
-              leading: const Icon(
-                Icons.logout_outlined,
-                color: Colors.red,
-              ),
-              title: const Text(
-                'Log out',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              onTap: () async {
-                await auth.signOut();
-                final provider =
-                    Provider.of<GoogleSignInProvider>(context, listen: false);
-                await provider.logout();
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (context) => const AuthModule(),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: MyDrawer(user: user, auth: auth),
       body: Container(
         width: double.infinity,
         decoration: const BoxDecoration(
---
+++ lib/screens/player_screen/widgets/custo_drawer.dart
@@ -0,0 +1,113 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../auth_screen/auth_screen.dart';
+import '../../auth_screen/data/google_signin.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({
+    super.key,
+    required this.user,
+    required this.auth,
+  });
+
+  final User? user;
+  final FirebaseAuth auth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: const Color(0xFF144771),
+      elevation: 100,
+      child: ListView(
+        physics: const BouncingScrollPhysics(),
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            decoration: const BoxDecoration(color: Color(0xFF071A2C)),
+            accountName: Text(
+              user!.displayName ?? '',
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            accountEmail: Text(
+              'Email: ${user!.email ?? ''}',
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            currentAccountPicture: CircleAvatar(
+              foregroundColor: Colors.white,
+              radius: 40,
+              backgroundImage: user!.photoURL != null
+                  ? NetworkImage(user!.photoURL!)
+                  : const NetworkImage(
+                      'https://previews.123rf.com/images/tuktukdesign/tuktukdesign1606/tuktukdesign160600119/59070200-user-icon-man-profil-homme-d-affaires-avatar-personne-ic%C3%B4ne-illustration-vectorielle.jpg',
+                    ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.settings_outlined,
+              color: Colors.white,
+            ),
+            title: const Text(
+              'Account Settings',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onTap: () {
+              // Add navigation code here
+            },
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.info_outline,
+              color: Colors.white,
+            ),
+            title: const Text(
+              'Help',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onTap: () {
+              // Add navigation code here
+            },
+          ),
+          const SizedBox(height: 10),
+          const Divider(color: Colors.white),
+          const SizedBox(height: 10),
+          ListTile(
+            leading: const Icon(
+              Icons.logout_outlined,
+              color: Colors.red,
+            ),
+            title: const Text(
+              'Log out',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onTap: () async {
+              await auth.signOut();
+              final provider =
+                  Provider.of<GoogleSignInProvider>(context, listen: false);
+              await provider.logout();
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => const AuthModule(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
